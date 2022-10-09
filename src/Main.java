bc070d74cbed9ed0f2e4a6cd47064f0a40233f83
olgatut <lelka.tutskaya@yandex.ru>
2022-10-09 18:20:36 +0300

    Домашнее задание ЦИКЛЫ 1

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+    }
+    private static void task1() {
+        System.out.println("ЗАДАЧА №1");
+        System.out.println(" ");
+        for (int i = 1; i <= 10; i++){
+        System.out.println(i);
+        }
+        System.out.println(" ");
+    }
+    private static void task2() {
+        System.out.println("ЗАДАЧА №2");
+        System.out.println(" ");
+        for (int i = 10; i >= 1; i--){
+            System.out.println(i);
+        }
+        System.out.println(" ");
+    }
+    private static void task3() {
+        System.out.println("ЗАДАЧА №3");
+        System.out.println(" ");
+        for (int i = 0; i <= 17; i = i + 2){
+            System.out.println(i);
+        }
+        System.out.println(" ");
+    }
+    private static void task4() {
+        System.out.println("ЗАДАЧА №4");
+        System.out.println(" ");
+        for (int i = 10; i >= -10; i--){
+            System.out.println(i);
+        }
+        System.out.println(" ");
+    }
+    private static void task5() {
+        System.out.println("ЗАДАЧА №5");
+        System.out.println(" ");
+        for (int i = 1904; i < 2096; i = i + 4){
+            System.out.println(i);
+        }
+        System.out.println(" ");
+}
+    private static void task6() {
+        System.out.println("ЗАДАЧА №6");
+        System.out.println(" ");
+        for (int i = 0; i < 99; i = i + 7){
+            System.out.println(i);
+        }
+        System.out.println(" ");
+    }
+    private static void task7() {
+        System.out.println("ЗАДАЧА №7");
+        System.out.println(" ");
+    }
+}
+
+
+
+
+
